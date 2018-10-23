df3742ae42e97bbf3e55e8363318a958e754f19d
yulu <331700915@qq.com>
2018-10-23 13:59:46 +0800

    修改

--- 动画Test/ViewController.m
+++ 动画Test/ViewController.m
@@ -25,6 +25,8 @@
     [self.view.layer addSublayer:emitter];
     emitter.renderMode = kCAEmitterLayerUnordered;
     emitter.emitterPosition = CGPointMake(emitter.frame.size.width / 3.0, -75);
+    
+    
 }
 
 - (IBAction)start:(id)sender {
