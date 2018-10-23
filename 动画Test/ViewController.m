6040e3e6e1772d2a5e93ada23fae26a5b90e0612
yulu <331700915@qq.com>
2018-10-23 11:29:50 +0800

    提交内容

--- 动画Test/ViewController.m
+++ 动画Test/ViewController.m
@@ -9,16 +9,44 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    CAEmitterLayer *emitter;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     // Do any additional setup after loading the view, typically from a nib.
+    emitter = [CAEmitterLayer layer];
+    emitter.frame = self.view.bounds;
+    [self.view.layer addSublayer:emitter];
+    emitter.renderMode = kCAEmitterLayerUnordered;
+    emitter.emitterPosition = CGPointMake(emitter.frame.size.width / 3.0, -75);
 }
 
+- (IBAction)start:(id)sender {
+   
+    CAEmitterCell *cell = [[CAEmitterCell alloc] init];
+    cell.contents = (__bridge id)[UIImage imageNamed:@"雪花"].CGImage;
+    cell.birthRate = 5;
+    cell.lifetime =7.0 ;
+    cell.alphaSpeed = 0;
+    cell.velocity = 150;
+    cell.velocityRange = 100;
+    cell.emissionLongitude = M_PI / 2;
+    cell.emissionRange = M_PI /2 ;
+    emitter.emitterCells = @[cell];
+    
+}
+- (IBAction)end:(id)sender {
+    
+    
+    emitter.emitterCells=nil;
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
